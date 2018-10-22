c4d395141c5363f861fcf2b251c66d3938f7015f
Tomáš Čížek <tomascizek@gmail.com>
2018-10-22 13:24:18 +0200

    Implement CashMachineWithholdPolicy

---
+++ src/CardManagement/Policy/CashMachineWithholdPolicy.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace CashMachine\CardManagement\Policy;
+
+use CashMachine\CardManagement\DomainModel\Card\WithholdMoney\WithholdAmountFromCardCommand;
+use CashMachine\CardManagement\DomainModel\Card\WithholdMoney\WithholdAmountFromCardCommandHandler;
+use CashMachine\CashMachine\DomainModel\CashMachine\RequestMoney\MoneyRequestFromCashMachineAccepted;
+
+final class CashMachineWithholdPolicy
+{
+	/**
+	 * @var WithholdAmountFromCardCommandHandler
+	 */
+	private $handler;
+
+	public function __construct(WithholdAmountFromCardCommandHandler $withholdAmountFromCardCommandHandler)
+	{
+		$this->handler = $withholdAmountFromCardCommandHandler;
+	}
+
+	public function process(MoneyRequestFromCashMachineAccepted $event): void
+	{
+		$command = WithholdAmountFromCardCommand::fromValues(
+			$event->getCardId(),
+			$event->getAmount()
+		);
+		$this->handler->handle($command);
+	}
+}
